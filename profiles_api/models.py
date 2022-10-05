fb80df4ccacb4851364a31c9e0f7b940351cdf48
Abhijeet <abhisharma129@gmail.com>
2022-10-05 15:31:45 +0530

    Added admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,14 +20,14 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using=sefl._db)
+        user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create superuser with vgiven detail"""
 
-        user = self.create_super_user(email, name, password)
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
