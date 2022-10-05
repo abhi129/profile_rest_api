5a48e4fffc51db84654dfc606f12488e91c6085d
Abhijeet <abhisharma129@gmail.com>
2022-10-05 14:58:47 +0530

    Added migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,60 @@
+import email
+from pyexpat import model
+from xml.dom import ValidationErr
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """Manipulate base user profile"""
+
+    def create_user(self, email, name, password=None):
+        """Create new user profile"""
+        if not email:
+            raise ValueError("User must have email")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=sefl._db)
+
+        return user
+
+    def create_super_user(self, email, name, password):
+        """Create superuser with vgiven detail"""
+
+        user = self.create_super_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """return full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Return short name of the use"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of user"""
+        return self.email
