e757eccddb6e217ca7e28df9e64888f6b95573bc
Abhijeet <abhisharma129@gmail.com>
2022-10-06 14:05:54 +0530

    Added crud

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test view api"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return the list of aPiView features"""
@@ -15,6 +19,28 @@ class HelloApiView(APIView):
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        """Create hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
 
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            meassage = f'hello {name}'
+            return Response({'message': meassage})
+        else:
+            return Response(serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
 
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial updating an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle deleting an object"""
+        return Response({'method': 'DELETE'})
 # Create your views here.
