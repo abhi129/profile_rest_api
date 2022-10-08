b6f7df2e7f68f77d4b7534bd994566958ab2cd29
Abhijeet <abhisharma129@gmail.com>
2022-10-08 19:51:10 +0530

    Added authorization

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,6 +8,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from profiles_api import permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 from profiles_api import serializers
@@ -104,5 +106,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
